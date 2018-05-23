23bc53f1ff5befd1fa641b7200f56b90e4bb7ec6
thanhquangit <thanhquang.it.ute@gmail.com>
2018-05-24 00:35:06 +0700

    update app

--- app/resources/views/page/blocks/advertisement_top_right.blade.php
+++
@@ -1,6 +0,0 @@
-<img width="280" src="images/1.png" />
-<div style="height:10px"></div>
-<img width="280" src="images/2.png" />
-<div style="height:10px"></div>
-<img width="280" src="images/3.png" />
-<div style="height:10px"></div>
