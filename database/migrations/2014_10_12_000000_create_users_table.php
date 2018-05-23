f626679ba30845d8e2bf7a037c0d60ba2cf16f0a
thanhquangit <thanhquang.it.ute@gmail.com>
2018-05-24 00:32:06 +0700

    update migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('username')->unique();
+            $table->string('password')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->integer('idGroup')->default(0);
+            $table->date('birthday')->nullable();
+            $table->integer('sex')->nullable();
+            $table->integer('active')->default('1');
+            $table->string('randomkey')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
